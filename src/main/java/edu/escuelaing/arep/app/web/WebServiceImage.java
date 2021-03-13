c5201f5b00c9abe5cf0c6a898d8f51aefc369690
Jairo Pulido <jairo.pulido@mail.escuelaing.edu.co>
2021-03-12 20:32:50 -0500

    Implementación código

---
+++ src/main/java/edu/escuelaing/arep/app/web/WebServiceImage.java
@@ -0,0 +1,16 @@
+package edu.escuelaing.arep.app.web;
+import edu.escuelaing.arep.app.anotaciones.Web;
+/**
+ * Clase que se encarga de realizar la respectiva ejecucion y despliegue del fondo de pantalla en formato HTML.
+ * @author  Jairo Pulido
+ */
+public class WebServiceImage {
+	/**
+     * Metodo que se encarga de ejecutar la anotacion @Web y retornar el contenido de la pagina HTML en el recurso /fondodepantalla.html.
+     * @return Retorna un fondo de pantalla en formato HTML.
+     */
+    @Web("/fondodepantalla.html")
+    public static String showImage() {
+        return "<html><title>Fondo de Pantalla</title><body><img src=\"/Wallpaper.jpg\" width=\"1900\" height=\"950\"></body></html>";
+    }
+}
